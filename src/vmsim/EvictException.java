b1d126eb053c02e066e77c5b221937f2bcef168a
Josh Gibbs <upaymeifixit@gmail.com>
2018-03-04 22:56:34 -0800

    Overloaded EvictException constructor

--- src/vmsim/EvictException.java
+++ src/vmsim/EvictException.java
@@ -3,6 +3,11 @@ package vmsim;
 public class EvictException extends Exception {
 	private String type;
 	private PageTableEntry pte;
+
+	EvictException (String str) {
+		type = str;
+	}
+
 	EvictException(String str, PageTableEntry pte) { 
 		type = str; 
 		this.pte = pte;
